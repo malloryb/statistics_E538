43d830170b684abf6ed408687c90a6ab9d789776
malloryb <mallorylbarnes@gmail.com>
2025-08-20 16:40:36 -0400

    render pdf too

--- global_stuff.R
+++ global_stuff.R
@@ -1,17 +1,11 @@
-# Make sure PDF companions are not created
+#Don't create PDF companions during HTML builds
 options(knitr.graphics.auto_pdf = FALSE)
 
 knitr::opts_chunk$set(
-    # write assets to docs/<chapter>_files/figure-<dev>*
-    fig.path = file.path(outdir, "figure-"),
-    dev      = "png",
-    fig.ext  = "png",
-    dpi      = 120,
-    # your existing defaults
-    collapse = TRUE,
-    comment  = "#>",
-    message  = FALSE,
-    warning  = FALSE,
-    out.width= "75%",
-    echo     = FALSE
-  )
+  collapse = TRUE,
+  comment = "#>",
+  message = FALSE,
+  warning = FALSE,
+  out.width = '75%',
+  echo = FALSE
+)
