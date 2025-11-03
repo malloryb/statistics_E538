2e4e92c19e3b7d41767467143aa3b7af2dfab471
malloryb <mallorylbarnes@gmail.com>
2025-11-03 09:58:41 -0500

    Moved kable setup to the global stuff file hopefuly to fix the cahpter title issue for Ch 10

--- global_stuff.R
+++ global_stuff.R
@@ -6,3 +6,29 @@ knitr::opts_chunk$set(
   out.width = '75%',
   echo = FALSE
 )
+
+suppressPackageStartupMessages({
+  library(knitr)
+  library(kableExtra)
+})
+
+knitr::opts_knit$set(allow_html = TRUE)  # allow HTML in PDF
+
+# 1) Mask kable() so it auto-chooses format per target
+kable <- function(x, format = NULL, ...) {
+  if (knitr::is_latex_output()) format <- "latex"
+  else if (is.null(format)) format <- "html"
+  knitr::kable(x, format = format, ...)
+}
+
+# 2) Make scroll_box() a no-op in PDF
+scroll_box <- function(x, ...) {
+  if (knitr::is_latex_output()) return(x)
+  kableExtra::scroll_box(x, ...)
+}
+
+if (knitr::is_latex_output()) {
+  options(knitr.table.format = "latex")
+} else {
+  options(knitr.table.format = "html")
+}
